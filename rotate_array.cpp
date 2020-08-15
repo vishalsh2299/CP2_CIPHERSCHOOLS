5ae097b575fd47aa535222920ed33f038906abaa
vishalsh2299 <vishalsh2299@gmail.com>
2020-08-15 23:22:25 +0530

    Rotate Array LeetCode

---
+++ rotate_array.cpp
@@ -0,0 +1,11 @@
+void rotate(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        a[(i + k) % n] = nums[i];
+    }
+    nums = a;
+}
